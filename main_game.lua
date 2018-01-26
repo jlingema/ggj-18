c56ca0c48503d48b99eb79c24a33af3e823722c2
Joschka Lingemann <j.lingemann@gmail.com>
2018-01-26 18:06:53 +0100

    Played around with rnd and stuff.

--- main_game.lua
+++ main_game.lua
@@ -1,13 +1,18 @@
 x = 64  y = 64
-
+scr_shk_str = 0
 function _update()
  if (btn(0)) then x=x-1 end
  if (btn(1)) then x=x+1 end
  if (btn(2)) then y=y-1 end
  if (btn(3)) then y=y+1 end
+ if (btn(4)) then scr_shk_str=scr_shk_str+4 end
+ if(scr_shk_str > 0.1) then  scr_shk_str=scr_shk_str*0.6
+ else scr_shk_str=0 end
 end
 
 function _draw()
- rectfill(0,0,127,127,5)
- circfill(x,y,7,14)
+ x_random = (rnd (scr_shk_str*2)) - scr_shk_str
+ y_random = (rnd (scr_shk_str*2)) - scr_shk_str
+ rectfill(0,0,127,127,1)
+ circfill((x+x_random)%127,(y+y_random)%127,2,4)
 end
