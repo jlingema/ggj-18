d42adb7bc85b5679fb5eb9e824d3a3908aec9b1f
Joschka Poettgen <j.lingemann@gmail.com>
2018-01-26 12:56:31 +0100

    Take an env variable instead of hardcoded path, update README

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
-REM this assumes that we have the picotool in ../picotool
+REM requires you to set the environment variable %picotoolpath%
 echo Building.
-py -3 ..\picotool\p8tool build game.p8.png --lua main_game.lua
+
+py -3 %picotoolpath%\p8tool build game.p8.png --lua main_game.lua
 IF %ERRORLEVEL% EQU 0 (
     echo Success. Copying cartridge.
     cp game.p8.png %appdata%\pico-8\carts\game.p8.png
