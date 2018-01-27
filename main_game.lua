980f880f0112a578e51b873cc15a3f8235d8aebc
Joschka Lingemann <j.lingemann@gmail.com>
2018-01-27 09:32:06 +0100

    Add a 'ground'.

--- main_game.lua
+++ main_game.lua
@@ -1,10 +1,14 @@
-x = 64  y = 64
+plr_x = 64
+cam_x = 0
+y = 128-32
 scr_shk_str = 0
+
+
 function _update()
- if (btn(0)) then x=x-1 end
- if (btn(1)) then x=x+1 end
- if (btn(2)) then y=y-1 end
- if (btn(3)) then y=y+1 end
+ if (btn(0)) then plr_x=plr_x-1 end
+ if (btn(1)) then plr_x=plr_x+1 end
+ --if (btn(2)) then y=y-1 end
+ --if (btn(3)) then y=y+1 end
  if (btn(4)) then scr_shk_str=scr_shk_str+4 end
  if(scr_shk_str > 0.1) then  scr_shk_str=scr_shk_str*0.6
  else scr_shk_str=0 end
@@ -14,5 +18,6 @@ function _draw()
  x_random = (rnd (scr_shk_str*2)) - scr_shk_str
  y_random = (rnd (scr_shk_str*2)) - scr_shk_str
  rectfill(0,0,127,127,1)
- circfill((x+x_random)%127,(y+y_random)%127,2,4)
+ rectfill(0,(99+y_random),127,127,2)
+ circfill((plr_x+x_random)%127,(y+y_random)%127,2,4)
 end
