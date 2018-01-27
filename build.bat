f005bc08c91186315996e89a196749eb85c573f0
Joschka Lingemann <j.lingemann@gmail.com>
2018-01-27 09:48:37 +0100

    make build.bat actually windows compatible

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ echo Building.
 py -3 %picotoolpath%\p8tool build game.p8.png --lua main_game.lua
 IF %ERRORLEVEL% EQU 0 (
     echo Success. Copying cartridge.
-    cp game.p8.png %appdata%\pico-8\carts\game.p8.png
+    xcopy /y game.p8.png %appdata%\pico-8\carts\game.p8.png
 )
