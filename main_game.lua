da6f208d4c1366606e072d31ef484e415adbfb1f
Joschka Lingemann <j.lingemann@gmail.com>
2018-01-27 11:15:04 +0100

    It was so nice here.

--- main_game.lua
+++ main_game.lua
@@ -1,15 +1,21 @@
-plr_x = 64
-plr_y = 100
+stone_x = 64
+stone_y = 100
 cam_x = 0
 y = 128-32
 
 -- GameData = require('data')
 Camera = require('camera')
-Camera = require('player')
+Player = require('player')
 
 function _update()
- if (btn(0)) then Camera.move(-1, 0) end
- if (btn(1)) then Camera.move(1, 0) end
+ if (btn(0)) then
+    Camera.move(-1, 0)
+    Player.move(-1, 0)
+ end
+ if (btn(1)) then
+    Camera.move(1, 0)
+    Player.move(1, 0)
+ end
  --if (btn(2)) then Camera.move(0, -1) end
  --if (btn(3)) then Camera.move(0, 1) end
  if (btn(4)) then Camera.shake() end
@@ -17,7 +23,8 @@ function _update()
 end
 
 function _draw()
- rectfill(0,0,127,127,1)
- rectfill(0,99,127,127,2)
- circfill(plr_x%127,plr_y%127,2,4)
+ rectfill(0,0,127,127,1) --the sky
+ rectfill(0,99,127,127,2) --the floor
+ circfill(stone_x%127,stone_y%127,2,4) --a stone
+ Player.draw()
 end
